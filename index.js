58f6c935ad9760178e87de7a51a8c04235a56743
alex <alex.giaffreda7@gmail.com>
2022-12-14 12:17:41 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+class State {
+  constructor(initialState, callback) {
+    this.initialState = { value: initialState };
+    this.callback = typeof callback === "function" ? callback : () => {};
+  }
+
+  get state() {
+    return this.initialState.value;
+  }
+
+  // set state(value){
+  //     this.initialState.value = {
+  //        ...this.initialState.value,
+  //        ...value
+  //     }
+  //     this.callback(this.initialState.value)
+  //     return true
+  // }
+
+  setState = (value) => {
+    this.initialState.value = {
+      ...this.initialState.value,
+      ...value,
+    };
+
+    this.callback(this.initialState.value)
+  };
+}
+
+const memory = new State({ counter: 0 }, (newState) => {
+  console.log(newState);
+});
+
+memory.setState({counter: memory.state.counter + 1})
+
+/* memory.state.counter = memory.state.counter + 1; */
+
+/*  memory.state = {counter: memory.state.counter + 1}; */
